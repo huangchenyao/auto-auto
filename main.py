da7fda47cbef8360458b2cea514766678ed85fd0
huangchenyao <hcy_96@outlook.com>
2019-09-27 09:15:34 +0800

    官方实现自动探险

--- main.py
+++ main.py
@@ -6,16 +6,3 @@ if __name__ == '__main__':
     png_name = 'liao_li_wang'
 
     Liaoliwang(path, png_name).ad_auto()
-    # Liaoliwang(path, png_name).adventure_auto()
-
-    # cv2.imwrite('./screenshot/tmp2.png', img1)
-
-    # adventure_map = [
-    #     [0, 0, 0, 0],
-    #     [0, 0, 0, 0],
-    #     [0, 0, 0, 0],
-    #     [0, 0, 0, 0],
-    #     [0, 0, 0, 0],
-    # ]
-    # shape = [[1, 1, 1]]
-    # find(adventure_map, shape)
