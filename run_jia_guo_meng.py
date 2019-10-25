bd133cfa66b4e042eacd279e44db8a31864f99e9
huangchenyao <hcy_96@outlook.com>
2019-10-25 14:36:22 +0800

    增加强国煤业

--- run_jia_guo_meng.py
+++ run_jia_guo_meng.py
@@ -1,8 +1,11 @@
 # -*- coding:utf-8 -*-
 from jiaguomeng.Jiaguomeng import Jiaguomeng
+import tkinter.messagebox
 
 if __name__ == '__main__':
-    # Jiaguomeng().update(60 * 1000)
+    # Jiaguomeng().update(120 * 1000)
     # Jiaguomeng().train_template(True)
     Jiaguomeng(level=1).train()
-    # Jiaguomeng().open('20', 50)
+    # Jiaguomeng().open('album', 500)
+
+    tkinter.messagebox.showinfo('提示', 'Finish!')
